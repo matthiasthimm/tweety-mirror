d0a831df98e2821cf33a86165629d5a424c93bd3
mthimm <mthimm@2e97e4f4-a37b-4b59-a545-73578c90adc5>
2012-12-07 16:18:54 +0000

    extended evaluation framework

--- projects/net-sf-tweety-agents-dialoguesystems/src/net/sf/tweety/agents/argumentation/test/GroundedTest.java
+++ projects/net-sf-tweety-agents-dialoguesystems/src/net/sf/tweety/agents/argumentation/test/GroundedTest.java
@@ -27,6 +27,13 @@ import net.sf.tweety.argumentation.util.DungTheoryGenerator;
 
 public class GroundedTest {
 	
+	//Global parameters for simulation
+	public static int frameworkSize;
+	public static double attackProbability;
+	public static boolean enforceTreeShape;
+	
+	public static int numberOfRunsEach = 100;
+	
 	/**
 	 * This method shows that with increasing complexity of the T1-belief state of
 	 * the CONTRA agent (and constant model of the PRO agent), the average utility of
@@ -42,9 +49,9 @@ public class GroundedTest {
 			// be in the grounded extension (so under perfect information, the PRO
 			// agent should always win)
 			DungTheoryGenerationParameters params = new DungTheoryGenerationParameters();
-			params.attackProbability = 0.3;
-			params.numberOfArguments = 15;	
-			params.enforceTreeShape = true;	
+			params.attackProbability = GroundedTest.attackProbability;
+			params.numberOfArguments = GroundedTest.frameworkSize;	
+			params.enforceTreeShape = GroundedTest.enforceTreeShape;	
 			DungTheoryGenerator gen = new DefaultDungTheoryGenerator(params);
 			// PRO agent knows 50% of all arguments, CONTRA agent knows 90% of all arguments
 			MultiAgentSystemGenerator<ArguingAgent,GroundedGameSystem> masGenerator = new GroundedGameGenerator(gen, 0.5, 0.9);
@@ -68,86 +75,123 @@ public class GroundedTest {
 			ProtocolGenerator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> protGenerator = new GroundedGameProtocolGenerator();
 			GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> sim = new GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem>(masGenerator,protGenerator,agentGenerators);
 			// Run iterated simulations and show aggregated results
-			SimulationResult<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> result = sim.run(100);
-			System.out.println("=================");
-			System.out.println("Depth of CONTRA agent model: " + i);
+			SimulationResult<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> result = sim.run(GroundedTest.numberOfRunsEach);
+			System.out.println("================= T1 vs T1 ==== " + GroundedTest.frameworkSize + " arguments, " + GroundedTest.attackProbability + " attack probability, " + (GroundedTest.enforceTreeShape?("tree shape"):("no tree shape")) + " ==========");
+			System.out.println("Depth of CONTRA agent model (T1): " + i);
 			System.out.println(result.display());
 		}
 	}
 	
+	/**
+	 * Same as runSimulationT1()
+	 * @throws ProtocolTerminatedException
+	 */
 	public static void runSimulationT2() throws ProtocolTerminatedException{
-		// TODO: plausibility check
-		DungTheoryGenerationParameters params = new DungTheoryGenerationParameters();
-		params.attackProbability = 0.3;
-		params.numberOfArguments = 5;			
-		params.enforceTreeShape = true;	
-		DungTheoryGenerator gen = new DefaultDungTheoryGenerator(params);
-		// PRO agent knows 50% of all arguments, CONTRA agent knows 90% of all arguments
-		MultiAgentSystemGenerator<ArguingAgent,GroundedGameSystem> masGenerator = new GroundedGameGenerator(gen, 0.5, 0.9);
-		List<AgentGenerator<ArguingAgent,GroundedGameSystem>> agentGenerators = new ArrayList<AgentGenerator<ArguingAgent,GroundedGameSystem>>();
+		// We run different simulations with increasing complexity
+		// of the CON agent's belief state
+		int depth[] = {1,2,3,4};				//depth complexity
+		double decay[] = {0.4,0.3,0.2,0.1};		//decay complexity
+		int width[] = {4,3,2,2};				//width complexity
+		for(int i = 0; i < 3; i++){
+			DungTheoryGenerationParameters params = new DungTheoryGenerationParameters();
+			params.attackProbability = GroundedTest.attackProbability;
+			params.numberOfArguments = GroundedTest.frameworkSize;			
+			params.enforceTreeShape = GroundedTest.enforceTreeShape;	
+			DungTheoryGenerator gen = new DefaultDungTheoryGenerator(params);
+			// PRO agent knows 50% of all arguments, CONTRA agent knows 90% of all arguments
+			MultiAgentSystemGenerator<ArguingAgent,GroundedGameSystem> masGenerator = new GroundedGameGenerator(gen, 0.5, 0.9);
+			List<AgentGenerator<ArguingAgent,GroundedGameSystem>> agentGenerators = new ArrayList<AgentGenerator<ArguingAgent,GroundedGameSystem>>();
 		
-		// The PRO agent has a T1 belief state without opponent model
-		T1Configuration configPro = new T1Configuration();
-		configPro.maxRecursionDepth = 0;
-		configPro.probRecursionDecay = 0;
-		configPro.oppModelCorrect = true;
-		// The CONTRA agent has a T2 belief state
-		T2Configuration configCon = new T2Configuration();
-		configCon.maxRecursionDepth = 1;
-		configCon.probRecursionDecay = 0.3;
-		configCon.maxRecursionWidth = 3;
+			// The PRO agent has a T1 belief state without opponent model
+			T1Configuration configPro = new T1Configuration();
+			configPro.maxRecursionDepth = 0;
+			configPro.probRecursionDecay = 0;
+			configPro.oppModelCorrect = true;
+			// The CONTRA agent has a T2 belief state of complexity i
+			T2Configuration configCon = new T2Configuration();
+			configCon.maxRecursionDepth = depth[i];
+			configCon.probRecursionDecay = decay[i];
+			configCon.maxRecursionWidth = width[i];
 		
-		agentGenerators.add(new GroundedGameT1AgentGenerator(GroundedGameSystem.AgentFaction.PRO,configPro));
-		agentGenerators.add(new GroundedGameT2AgentGenerator(GroundedGameSystem.AgentFaction.CONTRA,configCon));
-		
-		ProtocolGenerator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> protGenerator = new GroundedGameProtocolGenerator();
-		GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> sim = new GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem>(masGenerator,protGenerator,agentGenerators);
-		// run iterated simulations and show aggregated results
-		SimulationResult<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> result = sim.run(1);		
-		System.out.println(result.display());
+			agentGenerators.add(new GroundedGameT1AgentGenerator(GroundedGameSystem.AgentFaction.PRO,configPro));
+			agentGenerators.add(new GroundedGameT2AgentGenerator(GroundedGameSystem.AgentFaction.CONTRA,configCon));
+			
+			ProtocolGenerator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> protGenerator = new GroundedGameProtocolGenerator();
+			GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> sim = new GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem>(masGenerator,protGenerator,agentGenerators);
+			// run iterated simulations and show aggregated results
+			SimulationResult<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> result = sim.run(GroundedTest.numberOfRunsEach);		
+			System.out.println("================= T1 vs T2 ==== " + GroundedTest.frameworkSize + " arguments, " + GroundedTest.attackProbability + " attack probability, " + (GroundedTest.enforceTreeShape?("tree shape"):("no tree shape")) + " ==========");			
+			System.out.println("Complexity of CONTRA agent model (T2): (" + depth[i] + "," + decay[i] + "," + width[i] + ")");
+			System.out.println(result.display());
+		}
 	}
 	
+	/**
+	 * Same as runSimulationT1()
+	 * @throws ProtocolTerminatedException
+	 */
 	public static void runSimulationT3() throws ProtocolTerminatedException{
-		// TODO: plausibility check
-		DungTheoryGenerationParameters params = new DungTheoryGenerationParameters();
-		params.attackProbability = 0.3;
-		params.numberOfArguments = 10;	
-		params.enforceTreeShape = true;
-		DungTheoryGenerator gen = new DefaultDungTheoryGenerator(params);
-		// PRO agent knows 50% of all arguments, CONTRA agent knows 90% of all arguments
-		MultiAgentSystemGenerator<ArguingAgent,GroundedGameSystem> masGenerator = new GroundedGameGenerator(gen, 0.5, 0.9);
-		List<AgentGenerator<ArguingAgent,GroundedGameSystem>> agentGenerators = new ArrayList<AgentGenerator<ArguingAgent,GroundedGameSystem>>();
+		// We run different simulations with increasing complexity
+		// of the CON agent's belief state
+		int depth[] = {1,2,3,4};				//depth complexity
+		double decay[] = {0.4,0.3,0.2,0.1};		//decay complexity
+		int width[] = {4,3,2,2};				//width complexity
+		double virtArg[] = {0.3,0.25,0.2,0.15};	//virtual arguments
+		double virtAtt[] = {0.6,0.7,0.8,0.9};	//virtual attacks
+		for(int i = 0; i < 3; i++){
+			DungTheoryGenerationParameters params = new DungTheoryGenerationParameters();
+			params.attackProbability = GroundedTest.attackProbability;
+			params.numberOfArguments = GroundedTest.frameworkSize;	
+			params.enforceTreeShape = GroundedTest.enforceTreeShape;
+			DungTheoryGenerator gen = new DefaultDungTheoryGenerator(params);
+			// PRO agent knows 50% of all arguments, CONTRA agent knows 90% of all arguments
+			MultiAgentSystemGenerator<ArguingAgent,GroundedGameSystem> masGenerator = new GroundedGameGenerator(gen, 0.5, 0.9);
+			List<AgentGenerator<ArguingAgent,GroundedGameSystem>> agentGenerators = new ArrayList<AgentGenerator<ArguingAgent,GroundedGameSystem>>();
 		
-		// The PRO agent has a T1 belief state without opponent model
-		T1Configuration configPro = new T1Configuration();
-		configPro.maxRecursionDepth = 0;
-		configPro.probRecursionDecay = 0;
-		configPro.oppModelCorrect = true;
-		// The CONTRA agent has a T3 belief state
-		T3Configuration configCon = new T3Configuration();
-		configCon.maxRecursionDepth = 2;
-		configCon.probRecursionDecay = 0.1;
-		configCon.maxRecursionWidth = 3;
-		configCon.percentageVirtualArguments = 0.2;
-		configCon.percentageVirtualAttacks = 0.9;
+			// The PRO agent has a T1 belief state without opponent model
+			T1Configuration configPro = new T1Configuration();
+			configPro.maxRecursionDepth = 0;
+			configPro.probRecursionDecay = 0;
+			configPro.oppModelCorrect = true;
+			// The CONTRA agent has a T3 belief state
+			T3Configuration configCon = new T3Configuration();
+			configCon.maxRecursionDepth = depth[i];
+			configCon.probRecursionDecay = decay[i];
+			configCon.maxRecursionWidth = width[i];
+			configCon.percentageVirtualArguments = virtArg[i];
+			configCon.percentageVirtualAttacks = virtAtt[i];
 				
-		agentGenerators.add(new GroundedGameT1AgentGenerator(GroundedGameSystem.AgentFaction.PRO,configPro));
-		agentGenerators.add(new GroundedGameT3AgentGenerator(GroundedGameSystem.AgentFaction.CONTRA,configCon));
+			agentGenerators.add(new GroundedGameT1AgentGenerator(GroundedGameSystem.AgentFaction.PRO,configPro));
+			agentGenerators.add(new GroundedGameT3AgentGenerator(GroundedGameSystem.AgentFaction.CONTRA,configCon));
 		
-		ProtocolGenerator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> protGenerator = new GroundedGameProtocolGenerator();
-		GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> sim = new GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem>(masGenerator,protGenerator,agentGenerators);
-		// run iterated simulations and show aggregated results
-		SimulationResult<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> result = sim.run(100);		
-		System.out.println(result.display());
+			ProtocolGenerator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> protGenerator = new GroundedGameProtocolGenerator();
+			GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> sim = new GameSimulator<GroundedGameProtocol,ArguingAgent,GroundedGameSystem>(masGenerator,protGenerator,agentGenerators);
+			// run iterated simulations and show aggregated results
+			SimulationResult<GroundedGameProtocol,ArguingAgent,GroundedGameSystem> result = sim.run(GroundedTest.numberOfRunsEach);
+			System.out.println("================= T1 vs T3 ==== " + GroundedTest.frameworkSize + " arguments, " + GroundedTest.attackProbability + " attack probability, " + (GroundedTest.enforceTreeShape?("tree shape"):("no tree shape")) + " ==========");
+			System.out.println("Complexity of CONTRA agent model (T3): (" + depth[i] + "," + decay[i] + "," + width[i] + "," + virtArg[i] + "," + virtAtt[i] + ")");
+			System.out.println(result.display());
+		}
 	}
 	
 	public static void main(String[] args) throws ProtocolTerminatedException{
 		// set logging level to "TRACE" to get detailed descriptions
 		TweetyLogging.logLevel = TweetyConfiguration.LogLevel.ERROR;
 		TweetyLogging.initLogging();
-		
-		GroundedTest.runSimulationT1();
-		//GroundedTest.runSimulationT2();
-		//GroundedTest.runSimulationT3();
+				
+		for(int numArguments = 10; numArguments <= 30; numArguments += 5){
+			GroundedTest.frameworkSize = numArguments;
+			for(double attackProbability = 0.2; attackProbability <= 0.4; attackProbability += 0.5){
+				GroundedTest.attackProbability = attackProbability;
+				GroundedTest.enforceTreeShape = true;
+				GroundedTest.runSimulationT1();
+				GroundedTest.runSimulationT2();
+				GroundedTest.runSimulationT3();
+				GroundedTest.enforceTreeShape = false;
+				GroundedTest.runSimulationT1();
+				GroundedTest.runSimulationT2();
+				GroundedTest.runSimulationT3();
+			}
+		}
 	}
 }
