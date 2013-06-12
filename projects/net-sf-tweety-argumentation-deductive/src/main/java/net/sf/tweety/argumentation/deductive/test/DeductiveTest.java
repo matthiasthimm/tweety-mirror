6722da953bc12ff216f148446292ca49df18164b
mthimm <mthimm@2e97e4f4-a37b-4b59-a545-73578c90adc5>
2013-06-12 09:47:37 +0000

    added a test class for deductive argumentation

---
+++ projects/net-sf-tweety-argumentation-deductive/src/main/java/net/sf/tweety/argumentation/deductive/test/DeductiveTest.java
@@ -0,0 +1,45 @@
+package net.sf.tweety.argumentation.deductive.test;
+
+import java.io.IOException;
+
+import net.sf.tweety.ParserException;
+import net.sf.tweety.argumentation.deductive.CompilationReasoner;
+import net.sf.tweety.argumentation.deductive.DeductiveKnowledgeBase;
+import net.sf.tweety.argumentation.deductive.accumulator.SimpleAccumulator;
+import net.sf.tweety.argumentation.deductive.categorizer.ClassicalCategorizer;
+import net.sf.tweety.logics.propositionallogic.parser.PlParser;
+import net.sf.tweety.logics.propositionallogic.syntax.PropositionalFormula;
+
+/**
+ * 
+ * For testing purposes.
+ * 
+ * @author Matthias Thimm
+ *
+ */
+public class DeductiveTest {
+
+	public static void main(String[] args) throws ParserException, IOException{
+		DeductiveKnowledgeBase kb = new DeductiveKnowledgeBase();
+		
+		PlParser parser = new PlParser();
+		kb.add((PropositionalFormula)parser.parseFormula("s"));
+		kb.add((PropositionalFormula)parser.parseFormula("!s || h"));
+		kb.add((PropositionalFormula)parser.parseFormula("l"));
+		kb.add((PropositionalFormula)parser.parseFormula("!l || m"));
+		kb.add((PropositionalFormula)parser.parseFormula("!m || h"));
+		kb.add((PropositionalFormula)parser.parseFormula("!m || !f"));
+		kb.add((PropositionalFormula)parser.parseFormula("f"));
+		kb.add((PropositionalFormula)parser.parseFormula("!f || !h"));
+		kb.add((PropositionalFormula)parser.parseFormula("v"));
+		kb.add((PropositionalFormula)parser.parseFormula("!v || !h"));
+		
+		System.out.println(kb);
+		
+		CompilationReasoner reasoner = new CompilationReasoner(kb, new ClassicalCategorizer(), new SimpleAccumulator());
+		
+		System.out.println(reasoner.query(parser.parseFormula("f")));
+		
+	}
+	
+}
