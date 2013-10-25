97001eca363f37072b2bb3ad6a0820fb513ea981
mthimm <mthimm@2e97e4f4-a37b-4b59-a545-73578c90adc5>
2013-10-25 07:24:18 +0000

    some thoughts on argument graph analysis

--- projects/net-sf-tweety-argumentation-dung/src/main/java/net/sf/tweety/argumentation/dung/test/AnalysisTest.java
+++ projects/net-sf-tweety-argumentation-dung/src/main/java/net/sf/tweety/argumentation/dung/test/AnalysisTest.java
@@ -39,6 +39,8 @@ public class AnalysisTest {
 			System.out.println();
 		}
 	}
+
+	// ========================================================================================================================================================================================================
 	
 	public static void PageRankTest(){
 		DungTheoryGenerator gen = new IsoSafeEnumeratingDungTheoryGenerator();
@@ -72,6 +74,8 @@ public class AnalysisTest {
 		}
 	}
 
+	// ========================================================================================================================================================================================================
+	
 	public static void PageRankTest2(){
 		GroundReasoner reasoner;
 		for(int i = 0; i < 100; i++){
@@ -98,6 +102,8 @@ public class AnalysisTest {
 		}
 	}
 	
+	// ========================================================================================================================================================================================================
+	
 	/**
 	 * Inverted PageRank
 	 */
@@ -116,17 +122,19 @@ public class AnalysisTest {
 		double sum;
 		Map<Node,Double> pageRanks_tmp;		
 		do{			
-			System.out.println(pageRanks);
+	//		for(int i = 0; i < m; i++)
+		//		System.out.print("A"+i + " = " + pageRanks.get(new Argument("A"+i)) + "\t\t");
+			//System.out.println();
 			maxDiff = 0;
 			pageRanks_tmp = new HashMap<Node,Double>();			
 			for(Node v: g){
 				sum = 0;
 				for(Node w: g.getParents(v)){
-					sum -= pageRanks.get(w)/g.getChildren(w).size();
+					sum += pageRanks.get(w) * 1/g.getChildren(w).size();
 				}
 				//for(Node w: sinks)
 				//	sum += pageRanks.get(w)/m;
-				pageRanks_tmp.put(v, ((1-dampingFactor)/m) + (dampingFactor * sum));						
+				pageRanks_tmp.put(v, ((1-dampingFactor)/m) + (dampingFactor * (1-sum)));						
 				maxDiff = Math.max(maxDiff, Math.abs(pageRanks.get(v)-pageRanks_tmp.get(v)));				
 			}
 			pageRanks = pageRanks_tmp;			
@@ -134,25 +142,127 @@ public class AnalysisTest {
 		return pageRanks.get(n);
 	}
 	
+	// ========================================================================================================================================================================================================
+	
 	public static void InvertedPageRankTest(){
 		
 		DungTheory theory = new DungTheory();
 		Argument prev = new Argument("A0");
 		theory.add(prev);
-		for(int j = 1; j <= 4; j++){
+		for(int j = 1; j <= 5; j++){
 			Argument next = new Argument("A"+j);
 			theory.add(next);
 			theory.add(new Attack(prev,next));
 			prev = next;
 		}
-		AnalysisTest.pageRankInverted(theory, prev, 0.75, 0.01);		
+		AnalysisTest.pageRankInverted(theory, prev, 0.99, 0.01);		
 	}
+
+	// ========================================================================================================================================================================================================
+	
+	public static void InvertedPageRankTest2(){
+		DungTheoryGenerator gen = new IsoSafeEnumeratingDungTheoryGenerator();
+		GroundReasoner reasoner;
+		for(int i = 0; i < 30; i++){
+			DungTheory theory = gen.generate();
+			// skip theories with selfloops for now
+			if(theory.hasSelfLoops()) continue;
+			reasoner = new GroundReasoner(theory);
+			Labeling lab = new Labeling(theory,reasoner.getExtensions().iterator().next());
+			System.out.println(theory);
+			for(Argument arg: theory){
+				System.out.print(arg + " " + lab.get(arg) + " " + AnalysisTest.pageRankInverted(theory, arg, 0.99, 0.01) + "\t");
+			}
+			System.out.println();
+			System.out.println("===============================================================================");
+			
+		}		
+	}
+	
+	// ========================================================================================================================================================================================================
+	
+	/*
+	 * Inverted HITS rank
+	 */
+	public static Double hitsRankInverted(Graph<? extends Node> g, Node n, double precision, boolean getAuth){
+		Map<Node,Double> auth = new HashMap<Node,Double>();
+		Map<Node,Double> hub = new HashMap<Node,Double>();
+		// init
+		for(Node v: g){
+			auth.put(v,1d);
+			hub.put(v,1d);			
+		}
+		// iterate
+		double maxDiff;
+		double sum;
+		double norm;
+		Map<Node,Double> auth_tmp, hub_tmp;		
+		do{						
+			maxDiff = 0;
+			norm = 0;
+			auth_tmp = new HashMap<Node,Double>();
+			for(Node v: g){
+				sum = 0;
+				for(Node w: g.getParents(v))
+					sum += hub.get(w);
+				auth_tmp.put(v, 1-sum);
+				norm += Math.pow(1-sum, 2);
+			}
+			norm = Math.sqrt(norm);
+			for(Node v: g){
+				auth_tmp.put(v, auth_tmp.get(v) / norm);
+				maxDiff = Math.max(maxDiff, Math.abs(auth.get(v)-auth_tmp.get(v)));
+			}
+			norm = 0;
+			hub_tmp = new HashMap<Node,Double>();
+			for(Node v: g){
+				sum = 0;
+				for(Node w: g.getChildren(v))
+					sum += auth.get(w);
+				hub_tmp.put(v, 1-sum);
+				norm += Math.pow(1-sum, 2);
+			}
+			norm = Math.sqrt(norm);
+			for(Node v: g){
+				hub_tmp.put(v, hub_tmp.get(v) / norm);
+				maxDiff = Math.max(maxDiff, Math.abs(hub.get(v)-hub_tmp.get(v)));
+			}
+			auth = auth_tmp;
+			hub = hub_tmp;			
+		}while(maxDiff > precision);	
+		return getAuth ? auth.get(n) : hub.get(n);		
+	}
+	
+	// ========================================================================================================================================================================================================
+	
+	public static void InvertedHITSRankTest(){
+		DungTheoryGenerator gen = new IsoSafeEnumeratingDungTheoryGenerator();
+		GroundReasoner reasoner;
+		for(int i = 0; i < 40; i++){
+			DungTheory theory = gen.generate();
+			// skip theories with selfloops for now
+			if(theory.hasSelfLoops()) continue;
+			reasoner = new GroundReasoner(theory);
+			Labeling lab = new Labeling(theory,reasoner.getExtensions().iterator().next());
+			System.out.println(theory);
+			for(Argument arg: theory){
+				System.out.print(arg + " " + lab.get(arg) + " " + AnalysisTest.hitsRankInverted(theory, arg, 0.01, true) + " " + AnalysisTest.hitsRankInverted(theory, arg, 0.01, false) + "\t");
+			}
+			System.out.println();
+			System.out.println("===============================================================================");
+			
+		}		
+	}
+	
+	// ========================================================================================================================================================================================================
 	
 	public static void main(String[] args){
 		//AnalysisTest.EigenvalueTest();
 		//AnalysisTest.PageRankTest();
 		//AnalysisTest.PageRankTest2();
-		AnalysisTest.InvertedPageRankTest();
+		//AnalysisTest.InvertedPageRankTest();
+		//AnalysisTest.InvertedPageRankTest2();
+		AnalysisTest.InvertedHITSRankTest();
 	}
 }
 
