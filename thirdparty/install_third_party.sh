5fe9b4cb30af245b8d3373fd7f1c91e211c64eee
mthimm <mthimm@2e97e4f4-a37b-4b59-a545-73578c90adc5>
2014-01-06 09:42:19 +0000

    added lpsolve install command to the unix shell script

--- thirdparty/install_third_party.sh
+++ thirdparty/install_third_party.sh
@@ -3,3 +3,4 @@ mvn install:install-file -Dfile="lib/net-sf-tweety-math/cream106.jar" -DgroupId=
 mvn install:install-file -Dfile="lib/net-sf-tweety-math/qoca.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="qoca" -Dversion="1.0.0" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-graphs/Jama-1.0.3.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jama" -Dversion="1.0.3" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-cli/jspf.core-1.0.2.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jspf" -Dversion="1.0.2" -Dpackaging="jar"
+mvn install:install-file -Dfile="lib/net-sf-tweety-math/lpsolve55j.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="lpsolve" -Dversion="5.5" -Dpackaging="jar"
