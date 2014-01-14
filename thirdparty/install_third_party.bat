d9b70ec7d828cbd0284cfadf60af31b5a1b15c2c
nicopotyka <nicopotyka@2e97e4f4-a37b-4b59-a545-73578c90adc5>
2014-01-14 10:19:47 +0000

    - added ojAlgo for quadratic programming

--- thirdparty/install_third_party.bat
+++ thirdparty/install_third_party.bat
@@ -21,3 +21,7 @@ if not "%ERRORLEVEL%" == "0" exit /b
 call mvn install:install-file -Dfile="lib/net-sf-tweety-math/lpsolve55j.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="lpsolve" -Dversion="5.5" -Dpackaging="jar"
 echo Exit Code = %ERRORLEVEL%
 if not "%ERRORLEVEL%" == "0" exit /b
+
+call mvn install:install-file -Dfile="lib/net-sf-tweety-math/ojalgo-35.0.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="ojalgo" -Dversion="35" -Dpackaging="jar"
+echo Exit Code = %ERRORLEVEL%
+if not "%ERRORLEVEL%" == "0" exit /b
--- thirdparty/install_third_party.sh
+++ thirdparty/install_third_party.sh
@@ -4,3 +4,4 @@ mvn install:install-file -Dfile="lib/net-sf-tweety-math/qoca.jar" -DgroupId="net
 mvn install:install-file -Dfile="lib/net-sf-tweety-graphs/Jama-1.0.3.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jama" -Dversion="1.0.3" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-cli/jspf.core-1.0.2.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jspf" -Dversion="1.0.2" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-math/lpsolve55j.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="lpsolve" -Dversion="5.5" -Dpackaging="jar"
+mvn install:install-file -Dfile="lib/net-sf-tweety-math/ojalgo-35.0.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="ojalgo" -Dversion="35" -Dpackaging="jar"
